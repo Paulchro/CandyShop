496fa027839368462b5970f0d8e5ba4b7026f0fb
paulchro <paulchro@hotmail.com>
2022-12-16 11:13:54 +0200

    Add project files.

---
+++ CandyShop3/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace CandyShop3.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public int? CategoryId { get; set; }
+        public virtual ICollection<Category> Categories { get; set; }
+        //public int? PictureId { get; set; }
+        public virtual ICollection<Picture> Pictures { get; set; }
+    }
+}
